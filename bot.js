69ad3698d9ef3feb1cd50f5b9dff6d189474c29e
kamal885 <43897372+kamal885@users.noreply.github.com>
2018-10-27 01:16:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,20 @@
-const Discord = require("discord.js")
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const YTDL = require('ytdl-core');
-const nodeopus = require('node-opus');
-const ffmpeg = require('ffmpeg');
-var servers = {};
-function play(connection, message, args) {
-  var server = servers[message.guild.id];
-  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
-  server.queue.shift();
-  server.dispatcher.on("end", function() {
-    if (server.queue[0]) play(connection, message);
-    else connection.disconnect();
-  });
-}
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('ready', () => {
+   console.log(----------------);
+      console.log(Desert Bot- Script By : Simple);
+        console.log(----------------);
+      console.log(ON ${client.guilds.size} Servers '     Script By : Simple);
+    console.log(----------------);
+  console.log(Logged in as ${client.user.tag}!);
+client.user.setGame(BoNdOk.,"http://twitch.tv/S-F%22)
+client.user.setStatus("dnd")
+});
 
 
-client.on('message', message =>{
-  if(message.content.startsWith('join')){
-    const voiceChannel = message.member.voiceChannel
-    voiceChannel.join();
-    message.channel.send("تم الأتصال بالروم الصوتي")
-}})
+client.login(process.env.BOT_TOKEN);
